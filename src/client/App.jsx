bcb99165be38f4742531a802afdbbffe85544dc1
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-02-05 22:52:47 -0500

    Set up React with a package.json.

---
+++ src/client/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+        <footer class="footer">
+            <div class="content has-text-centered is-family-code">
+                <strong>A3-Experiment - Joe Dobbelaar, Priyanka Narasimhan, Randy Huang, Matthew McAlarney</strong>
+            </div>
+        </footer>
+    </>
+  )
+}
+
+export default App
