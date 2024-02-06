e52a33bf2bb45753b8b488c5436c284213d54b0b
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-02-05 23:16:09 -0500

    Rearranged index.js and index.html to get React start script working.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,13 +2,11 @@ import React from "react"
 
 function App() {
   return (
-    <>
-        <footer class="footer">
-            <div class="content has-text-centered is-family-code">
-                <strong>A3-Experiment - Joe Dobbelaar, Priyanka Narasimhan, Randy Huang, Matthew McAlarney</strong>
-            </div>
-        </footer>
-    </>
+    <footer class="footer">
+        <div class="content has-text-centered is-family-code">
+            <strong>A3-Experiment - Joe Dobbelaar, Priyanka Narasimhan, Randy Huang, Matthew McAlarney</strong>
+        </div>
+    </footer>
   )
 }
 
