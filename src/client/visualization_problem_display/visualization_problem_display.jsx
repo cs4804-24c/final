92f18abc228b6b51f7fb5e502f1651ffa151dd74
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-02-07 21:42:58 -0500

    Added ids to each button and the answer input element.

--- src/client/visualization_problem_display/visualization_problem_display.jsx
+++ src/client/visualization_problem_display/visualization_problem_display.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 function VisualizationProblemDisplay() {
   const [visualizationTitle, setVisualizationTitle] = useState("First") //First is a placeholder here.
   const [imagePath, setImagePath] = useState("") //Will come into use once we have icon array images.
+  const [submittedAnswer, setSubmittedAnswer] = useState("")
   return (
     <section class = "section">
         <h1 class = "is-size-1 is-family-primary has-text-weight-bold">{visualizationTitle} Icon Array</h1>
@@ -13,19 +14,19 @@ function VisualizationProblemDisplay() {
             </div> 
             <div class = "media-content">
               <div class = "content">
+                <label class="label is-size-3 is-family-monospace">Example: What fraction of people caught COVID-19 in the sample to the left?</label>
                 <div class = "field">
-                  <label class="label is-size-3 is-family-monospace">Example: What fraction of people caught COVID-19 in the sample to the left?</label>
                     <div class="control">
-                      <input class="input is-size-5 is-family-sans-serif" type="text" placeholder="Type answer here" />
+                      <input class="input is-size-5 is-family-sans-serif" type="text" placeholder="Type answer here" id="userAnswer"/>
                     </div>
                 </div>
                 <div class = "field">
                   <div class = "control">
-                    <button class="button is-medium is-link is-family-code">Submit</button>
+                    <button class="button is-medium is-link is-family-code" id="submissionButton">Submit</button>
                   </div>
                 </div>
                 <div class = "block">
-                  <button class = "button is-medium is-warning has-text-black is-family-code">Next Question</button>
+                  <button class = "button is-medium is-warning has-text-black is-family-code" id="nextQuestion">Next Question</button>
                 </div>
               </div>
             </div>
