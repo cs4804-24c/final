1c0c4c23421ea987b75d7b541fe18ee1440891b1
r2pen2 <joedobbelaar@gmail.com>
2024-02-13 17:29:38 -0500

    Google Icon

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import VisualizationProblemDisplay from "./visualization_problem_display/visualization_problem_display"
-import { SignInButton, getCurrentUser } from "./api/auth";
+import { SignInButton, SignOutButton, getCurrentUser } from "./api/auth";
 
 function App() {
 
@@ -10,29 +10,45 @@ function App() {
   /** Get the current user on load */
   useEffect(() => { getCurrentUser(setCurrentUser); }, [])
 
+  /** Signature w/ title and authors */
+  const Signature = () => (
+    <div className="content has-text-centered has-text-weight-normal is-family-sans-serif">
+      A3-Icon Array Data Visualization Experiment - Joe Dobbelaar, Priyanka Narasimhan, Randy Huang,
+      Matthew McAlarney
+    </div>
+  )
+
   /** Footer component displaying credits and current user */
   const Footer = () => {
     if (!currentUser) { return; }
     return (
       <footer className="footer">
-        <div className="content has-text-centered has-text-weight-normal is-family-sans-serif">
-            A3-Icon Array Data Visualization Experiment - Joe Dobbelaar, Priyanka Narasimhan, Randy Huang,
-            Matthew McAlarney
-        </div>
+        <Signature />
         <div className="has-text-centered">Signed in as: {currentUser.displayName}</div>
       </footer>
     )
   }
 
-  const SignIn = () => { if (currentUser) { return; } return <SignInButton />; }
+  const SignIn = () => { 
+    if (currentUser) { return; } 
+    return (
+      <div style={{height:"100vh", display: "flex", flexDirection: "column", justifyContent: "center", background: "white"}}>
+        <Signature />
+        <SignInButton />
+      </div>
+    );
+  }
 
   const MainDisplay = () => { if (!currentUser) { return; } return <Router><VisualizationProblemDisplay /></Router>; }
+
+  const SignOut = () => { if (!currentUser) { return; } return <SignOutButton />; }
 
   return (
     <>
       <MainDisplay />
       <SignIn />
       <Footer />
+      <SignOut />
     </>
   )
 }
--- src/client/api/auth.js
+++ src/client/api/auth.js
@@ -1,10 +1,13 @@
 import { auth } from "./firebase";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+/** Style for a container that centers buttons */
+const containerStyle = {display: "flex", justifyContent: "center", alignItems: "center", width: "100%", background: "white"}
+
 /** Sign in the current user with Google */
 export async function signIn() {
   return new Promise((resolve, reject) => {
-    signInWithPopup(auth, new GoogleAuthProvider()).then((result) => { resolve(result); }).catch((error) => { console.error(error); reject(error); })
+    signInWithPopup(auth, new GoogleAuthProvider()).then((result) => { resolve(result); }).catch((error) => { console.error(error); })
   })
 }
 
@@ -14,6 +17,19 @@ export async function getCurrentUser(setter) { auth.onAuthStateChanged((user) =>
 /** SignIn button component */
 export function SignInButton() {
   return (
-    <button onClick={signIn}>SignIn</button>
+    <div style={containerStyle}>
+      <button onClick={signIn} className="button is-family-code">
+        <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo" style={{width: 30, aspectRatio: "1/1", marginRight: 10, userSelect: "none"}} />
+        Sign In With Google
+      </button>
+    </div>
+  )
+}
+
+export function SignOutButton() {
+  return (
+    <div style={containerStyle}>
+      <button onClick={() => { auth.signOut() }} className="button is-family-code">Sign Out</button>
+    </div>
   )
 }
