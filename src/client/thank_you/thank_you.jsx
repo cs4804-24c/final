0e14aa3365789cda39372ced71339734e653c5d2
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-02-15 17:15:33 -0500

    Modified minimum height styling of thank you content.

--- src/client/thank_you/thank_you.jsx
+++ src/client/thank_you/thank_you.jsx
@@ -8,7 +8,7 @@ function ThankYou() {
             <h1 className = "is-size-1 is-family-primary has-text-weight-bold has-text-centered">
                 Thank you!
             </h1>
-            <div className = "columns is-centered" style = {{minHeight: "100vh"}}>
+            <div className = "columns is-centered" style = {{minHeight: "75vh"}}>
                 <div className = "column">
                     <div className = "buttons is-centered mt-3">
                         <button className = "button is-medium is-warning has-text-black is-family-code" onClick={navigate("/")}>Take survey again</button>
