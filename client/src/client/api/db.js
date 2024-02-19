0e0c339b52305b5aef8bb30751f0d97d9ad387ca
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-02-18 21:59:41 -0500

    Implemented Cleveland-Mcgraw relative error calucation in db.js

--- client/src/client/api/db.js
+++ client/src/client/api/db.js
@@ -87,7 +87,7 @@ export class QuestionStat {
         this.endTime = Date.now();
         this.answerTime = (this.endTime - this.startTime) / 1000;
         this.actualError = Math.abs(this.correctAnswer - userAnswer);
-        this.relativeError = this.actualError / this.correctAnswer;
+        this.relativeError = (Math.log2(Math.abs(userAnswer - this.correctAnswer) + (1/8)) === -3) ? 0 : Math.log2(Math.abs(userAnswer - this.correctAnswer) + (1/8))
     }
 
     /** Make this {@link QuestionStat} JSON serializable for Firestore Database */
