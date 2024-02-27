fef34a307ca058b53cb99e3102961d23d0aef7e5
Andrew Salls <asalls2@wpi.edu>
2024-02-27 17:50:52 -0500

    Render chart on page

--- vBeeswarm.js
+++ vBeeswarm.js
@@ -1,13 +1,32 @@
-Plot.plot({
-  height: 900,
-  y: {line: true},
-  marks: [
-    Plot.dotY(flare, Plot.dodgeX({
-      y: "endfrac",
-      sort: "endfrac",
-      r: flare.map(d => (d["participantsizeindicator"] / 2) + 1),
-      title: "name",
-      fill: "currentColor"
-    }))
+window.onload = async () => {
+  let flare = [
+    {
+      endfrac: 1994.444,
+      participantsizeindicator: 3
+    },
+    {
+      endfrac: 1775.11,
+      participantsizeindicator: 4
+    },
+    {
+      endfrac: 1921,
+      participantsizeindicator: 100
+    }
   ]
-})
+
+  const plot = Plot.plot({
+    height: 900,
+    y: {line: true},
+    marks: [
+      Plot.dotY(flare, Plot.dodgeX({
+        y: "endfrac",
+        sort: "endfrac",
+        r: flare.map(d => (d["participantsizeindicator"] / 2) + 1),
+        title: "name",
+        fill: "currentColor"
+      }))
+    ]
+  })
+  
+  document.querySelector("#chart-display").append(plot);
+}
