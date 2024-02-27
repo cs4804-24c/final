e2a86da9d3aa6a979ac8f5f11621f19e12935a6c
Andrew Salls <asalls2@wpi.edu>
2024-02-27 18:05:05 -0500

    Color selection and editing axis for comparisons

--- vBeeswarm.js
+++ vBeeswarm.js
@@ -1,29 +1,56 @@
+function colorMap(d) {
+  // Replace type with actual column
+  switch(d["type"]) {
+    case "A":
+      return "#FF0000";
+    case "B":
+      return "#00FF00";
+    case "C":
+      return "#0000FF";
+    default:
+      return "#000000";
+  }
+};
+
 window.onload = async () => {
   let flare = [
     {
       endfrac: 1994.444,
-      participantsizeindicator: 3
+      participantsizeindicator: 3,
+      type: "A"
     },
     {
       endfrac: 1775.11,
-      participantsizeindicator: 4
+      participantsizeindicator: 4,
+      type: "C"
     },
     {
       endfrac: 1921,
-      participantsizeindicator: 100
+      participantsizeindicator: 100,
+      type: "B"
+    },
+    {
+      endfrac: 1921,
+      participantsizeindicator: 4,
+      type: "A"
+    },
+    {
+      endfrac: 1840,
+      participantsizeindicator: 6,
+      type: "EEEEEEEE"
     }
   ]
 
   const plot = Plot.plot({
     height: 900,
-    y: {line: true},
+    y: {line: true, ticks: 0, label: null, axis: "left"}, // Replace with axis: "right" for flipped version
     marks: [
       Plot.dotY(flare, Plot.dodgeX({
         y: "endfrac",
         sort: "endfrac",
         r: flare.map(d => (d["participantsizeindicator"] / 2) + 1),
         title: "name",
-        fill: "currentColor"
+        fill: flare.map(d => colorMap(d))
       }))
     ]
   })
